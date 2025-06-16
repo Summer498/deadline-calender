6feb642ac2dacd303c4033ad54387d3845f3a493
Summer498@gmail.com <subore02@gmail.com>
2025-06-16 12:50:03 +0900

    divide before and after finished

--- api/[year]/[month]/[day].ts
+++ api/[year]/[month]/[day].ts
@@ -3,22 +3,26 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 export default function handler(req: VercelRequest, res: VercelResponse) {
   const { year, month, day } = req.query;
 
-  // 与えられた日付 (月は0始まりなので-1)
-  const targetDate = new Date(Number(year), Number(month) - 1, Number(day));
-
   const now = new Date;
-  
-  const toDate = (t) => Date.UTC(t.getFullYear(), t.getMonth(), t.getDate()) / (1000 * 60 * 60 * 24);
 
+  // 与えられた日付 (月は0始まりなので-1)
   const target = Date.UTC(Number(year), Number(month) - 1, Number(day))
-  const today = Date.UTC(now.getFullYear(), now.getMonth(), now.getDate()) + (1000 * 60 * 60 * 9);
+  const today = Date.UTC(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours())
 
-  const diffDays = toDate(target - today);
+  const toDate = (ms: number) => Math.floor(ms / (1000 * 60 * 60 * 24));
+  const diffDays = toDate(target) - toDate(today);
+
+  function getBGColor(diff: number) {
+    if (diff < 0) { return "#80FF80"; }
+    if (diff === 0) { return "yellow"; }
+    if (diff > 0) { return "#cbcbcb"; }
+    return "#000";
+  }
 
   function getColor(diff: number) {
-    if (diff > 0) { return "#F0F8FF"; }
-    if (diff === 0) { return "yellow"; }
-    if (diff < 0) { return "#cbcbcb"; }
+    if (diff < 0) { return "#fff"; }
+    if (diff === 0) { return "#000"; }
+    if (diff > 0) { return "#000"; }
     return "#000";
   }
 
@@ -29,18 +33,18 @@ export default function handler(req: VercelRequest, res: VercelResponse) {
       `<foreignObject x="0" y="0" width="143" height="126">`,
       `<html xmlns="http://www.w3.org/1999/xhtml">`,
       `<style>`,
-      `* {background-color: ${getColor(diffDays)}}`,
+      `* {background-color: ${getBGColor(diffDays)}; color: ${getColor(diffDays)}}`,
       `foreignObject {font-weight: bold;}`,
       `.year {font-size: 32px;line-height: 34px;text-align: center;margin: 3px 0 9px 0;letter-spacing: 3px;}`,
       `.md {font-size: 32px;text-align: center;margin: 0;letter-spacing: 2px;line-height: 13px;}`,
       `span {font-size: 10px;}`,
       `.count {font-size: 51px;text-align: center;letter-spacing: -4px;margin: 0;}`,
-      `@media (prefers-color-scheme: dark) {p,* {color: white}}`,
+      //      `@media (prefers-color-scheme: dark) {p,* {color: white}}`,
       `</style>`,
       `<div>`,
       `<p class="year">${year}</p>`,
       `<p class="md">${month}-${day}</p>`,
-      `<p class="count">${Math.abs(diffDays)}<span>日</span></p>`,
+      `<p class="count">${Math.abs(diffDays)}<span>${diffDays < 0 ? "日前" : diffDays > 0 ? "日後" : "日"}</span></p>`,
       `</div>`,
       `</html>`,
       `</foreignObject>`,
